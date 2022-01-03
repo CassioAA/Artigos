091b8e6bdccbbd17caf4ef784beedc2a70b5ac93
CassioAlmeida <cassioalmeidaccti@gmail.com>
2022-01-03 11:00:29 -0300

    views, serializers and urls

--- Artigos/artigos_app/admin.py
+++ Artigos/artigos_app/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import Autor, Artigo
-
+from .models import Autor,Artigo
 # Register your models here.
+
 admin.site.register(Autor)
 
 class ArtigoAdmin(admin.ModelAdmin):
-    list_display = ('autor', 'titulo', 'publicado_em', 'atualizado_em')
+    list_display = ('autor','titulo','publicado_em','atualizado_em')
     class Meta:
         model = Artigo
 
-admin.site.register(Artigo, ArtigoAdmin)
+
+
+admin.site.register(Artigo,ArtigoAdmin)
