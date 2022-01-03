357e2a58e92592eb28856ebdd28a96a8d1269b39
CassioAlmeida <cassioalmeidaccti@gmail.com>
2022-01-03 19:58:58 -0300

    a request

--- Artigos/Artigos/urls.py
+++ Artigos/Artigos/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from artigos_app.views import index
 
 urlpatterns = [
+    path('',index,name='index'),
     path('admin/', admin.site.urls),
     path('artigos_app/',include('artigos_app.api.urls')),
     path('api-auth/', include('rest_framework.urls'))
